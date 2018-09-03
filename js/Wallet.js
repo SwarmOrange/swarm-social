b52de3cc3907a2d2a47f7a8cfe53b693ba105097
shadurin <igor.shadurin@gmail.com>
2018-09-03 17:03:08 +0300

    save and load my ethereum address

--- js/Wallet.js
+++ js/Wallet.js
@@ -7,8 +7,25 @@ class Wallet {
     init() {
         let self = this;
         $('#v-pills-wallet-tab').click(function (e) {
-            e.preventDefault();
-            self.main.alert('Not implemented');
+            if ('ethereumWallet' in self.main.blog.myProfile) {
+                $('#myEthereumWallet').val(self.main.blog.myProfile.ethereumWallet);
+            }
+        });
+
+        $('.wallet-save').click(function (e) {
+            $('#wallet-form').addClass("disabled-content");
+            let wallet = $('#myEthereumWallet').val();
+            if (!web3.isAddress(wallet)) {
+                $('#wallet-form').removeClass("disabled-content");
+                self.main.alert('Incorrect Ethereum address');
+                return;
+            }
+
+            self.main.blog.myProfile.ethereumWallet = wallet;
+            self.main.blog.saveProfile(self.main.blog.myProfile).then(function (response) {
+                $('#wallet-form').removeClass("disabled-content");
+                self.main.onAfterHashChange(response.data, true);
+            });
         });
     }
 }
