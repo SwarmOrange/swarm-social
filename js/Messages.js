20002020e1249f9f1f299989650fd18881b789fd
shadurin <igor.shadurin@gmail.com>
2018-09-04 15:36:52 +0300

    messages example

--- js/Messages.js
+++ js/Messages.js
@@ -20,6 +20,28 @@ class Messages {
                 $('#message-to-avatar').attr('src', 'img/swarm-avatar.jpg');
                 self.showMessageInput(true);
             })
+            .on('click', '.message-open-dialog', function (e) {
+                e.preventDefault();
+                return;
+                let userHash = $(this).attr('data-user-hash');
+                let messageDialog = $('.message-dialog');
+                messageDialog.html('');
+                self.showLoadingBar(true);
+                self.main.blog.getMessageInfo().then(function (response) {
+                    self.showLoadingBar(false);
+                    let data = response.data;
+                    let lastMessageId = 0;
+                    if (userHash in data) {
+                        lastMessageId = data[userHash].last_message_id;
+                    }
+
+                    if (lastMessageId > 0) {
+                        // todo load messages
+                    }
+                }).catch(function () {
+                    self.showLoadingBar(false);
+                });
+            })
             .on('click', '.messages-write-message', function (e) {
                 let userId = $(this).attr('data-user-id');
                 let avatar = self.main.swarm.getFullUrl('social/file/avatar/original.jpg', userId);
@@ -43,11 +65,25 @@ class Messages {
 
         $('#v-pills-messages-tab').click(function (e) {
             let usersList = $('.messages-users-list');
+            let messageDialogs = $('.message-dialogs');
             usersList.html('<button class="dropdown-item messages-enter-user-hash" type="button">Enter user hash</button>');
+            messageDialogs.html('');
             let users = self.main.blog.getIFollow();
             users.forEach(function (v) {
                 usersList.append('<button class="dropdown-item text-center messages-write-message" type="button" data-user-id="' + v + '"><img src="' + self.main.swarm.getFullUrl('social/file/avatar/original.jpg', v) + '" alt="" class="size-36"></button>');
-            })
+            });
+
+            self.main.blog.getMessageInfo().then(function (response) {
+                let data = response.data;
+                Object.keys(data).forEach(function (userHash) {
+                    let info = data[userHash];
+                    let avatar = self.main.swarm.getFullUrl('social/file/avatar/original.jpg', userHash);
+
+                    messageDialogs.append('<div class="message-dialog">' +
+                        '<p><img class="size-30" src="' + avatar + '"> <a class="message-open-dialog" href="#" data-user-hash="' + userHash + '">' + userHash + '</a></p>' +
+                        '</div>');
+                });
+            });
         });
     }
 
@@ -56,6 +92,15 @@ class Messages {
             $('#send-message-to').show();
         } else {
             $('#send-message-to').hide();
+        }
+    }
+
+    showLoadingBar(isShow) {
+        let selector = $('.messages-loader, .messages-loader > .loader-animation');
+        if (isShow) {
+            selector.show();
+        } else {
+            selector.hide('slow');
         }
     }
 }
