7066da961cc60d68f87168d956aee4a3b29a3890
shadurin <igor.shadurin@gmail.com>
2018-09-05 15:35:21 +0300

    import backuped files

--- js/Settings.js
+++ js/Settings.js
@@ -7,7 +7,60 @@ class Settings {
     init() {
         let self = this;
         $('#v-pills-settings-tab').click(function (e) {
-            self.setStatus('');
+            self.setExportStatus('');
+        });
+
+        $('#settings-import-file').on('change', function (evt) {
+            self.setImportStatus('');
+            let userFiles = [];
+            if (this.files && this.files.length === 1) {
+                function uploadFiles() {
+                    if (userFiles.length <= 0) {
+                        self.setImportStatus('Uploading complete! Refresh this page');
+
+                        return;
+                    }
+
+                    let uploadFile = userFiles.shift();
+                    uploadFile.async('blob').then(function (content) {
+                        self.setImportStatus('Uploading file: ' + uploadFile.name + '...');
+                        return self.main.swarm.post(uploadFile.name, content).then(function (response) {
+                            self.main.onAfterHashChange(response.data, true);
+                            uploadFiles();
+                        });
+                    });
+                }
+
+                function handleFile(f) {
+                    JSZip.loadAsync(f).then(function (zip) {
+                        let isProfileFound = false;
+                        zip.forEach(function (relativePath, zipEntry) {
+                            if (zipEntry.name === "social/profile.json") {
+                                isProfileFound = true;
+                            }
+                            if (!zipEntry.dir) {
+                                userFiles.push(zipEntry);
+                            }
+                        });
+
+                        if (isProfileFound) {
+                            uploadFiles();
+
+                        } else {
+                            self.main.alert('Incorrect archive');
+                        }
+                    }, function (e) {
+                        self.setImportStatus("Error reading " + f.name + ": " + e.message);
+                    });
+                }
+
+                handleFile(evt.target.files[0]);
+            }
+        });
+
+        $('.settings-import-data').click(function (e) {
+            e.preventDefault();
+            $('#settings-import-file').click();
         });
 
         $('.settings-export-all-data').click(function (e) {
@@ -38,17 +91,21 @@ class Settings {
                 });
             };
 
-            self.setStatus('Find all files...');
+            self.setExportStatus('Find all files...');
             self.findAllFiles(null, null, function (files) {
-                self.setStatus('Found ' + files.length + ' files');
+                self.setExportStatus('Found ' + files.length + ' files. Downloading all to zip archive..');
                 receivedFiles = files;
                 downloadNext();
             });
         });
     }
 
-    setStatus(text) {
+    setExportStatus(text) {
         $('.export-status').html(text);
+    }
+
+    setImportStatus(text) {
+        $('.import-status').html(text);
     }
 
     findAllFiles(foundFiles, foundDirectories, onComplete) {
