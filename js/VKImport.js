a78a43ff061bf4b35c92b6171a15b2c9a715ff5f
shadurin <igor.shadurin@gmail.com>
2018-09-06 18:49:04 +0300

    prepare to import all info from vk

--- js/VKImport.js
+++ js/VKImport.js
@@ -7,11 +7,24 @@ class VKImport {
             currentPhotoAlbum: 0
         };
         this.main = main;
+        this.userInfo = {};
         this.init()
     }
 
     init() {
         let self = this;
+        $('.vk-select-photos').click(function (e) {
+            e.preventDefault();
+            $('.import-vk-content ul').addClass('hide');
+            $('.vk-list-photos').removeClass('hide')
+        });
+
+        $('.vk-select-videos').click(function (e) {
+            e.preventDefault();
+            $('.import-vk-content ul').addClass('hide');
+            $('.vk-list-videos').removeClass('hide')
+        });
+
         $('.btn-profile-import-vk').click(function (e) {
             e.preventDefault();
             $('#receiveVkPhotos').text('');
@@ -85,29 +98,96 @@ class VKImport {
     }
 
     vkAuthInfo(response) {
+        let self = this;
         console.log(response);
         if (response.session) {
-            console.log(response);
             let id = response.session.mid;
             console.log(id);
-            let vkContent = $('.vk-list');
+            let vkListPhotos = $('.vk-list-photos');
+            let vkListVideos = $('.vk-list-videos');
+            $('.vk-authorized').removeClass('hide');
+            $('.btn-vk-authorize').addClass('hide');
             VK.Api.call('photos.getAlbums', {owner_id: id, photo_sizes: 1, need_covers: '1', v: '5.80'}, function (r) {
                 console.log(r);
                 if (r.response) {
                     let albums = r.response.items;
-                    vkContent.html();
+                    vkListPhotos.html('');
                     albums.forEach(function (v) {
                         let thumb = v.sizes.length >= 4 ? v.sizes[3].src : v.sizes[v.sizes.length - 1].src;
-                        vkContent.append('<li class="list-inline-item col-sm-3">' +
-                            '<p><img class="vk-album-img" src="' + thumb + '"></p>' +
-                            '<p><button type="button" class="btn btn-primary btn-sm btn-import-vk-album" data-album-id="' + v.id + '" data-owner-id="' + v.owner_id + '">Import</button></p>' +
+                        vkListPhotos.append('<li class="list-inline-item col-sm-3" style="margin-bottom: 18px">' +
+                            '<img class="vk-album-img" src="' + thumb + '">' +
+                            //'<p><button type="button" class="btn btn-primary btn-sm btn-import-vk-album" data-album-id="' + v.id + '" data-owner-id="' + v.owner_id + '">Import</button></p>' +
+                            '<div class="form-check form-check-inline"><input class="form-check-input vk-checkbox-photo" type="checkbox" data-album-id="' + v.id + '" data-owner-id="' + v.owner_id + '" checked></div>' +
                             '</li>');
                     });
+                }
+            });
+
+            VK.Api.call('video.getAlbums', {owner_id: id, extended: '1', v: '5.80'}, function (r) {
+                console.log(r);
+                if (r.response) {
+                    let albums = r.response.items;
+                    vkListVideos.html('');
+                    albums.forEach(function (v) {
+                        let thumb = v.photo_320;
+                        vkListVideos.append('<li class="list-inline-item col-sm-3" style="margin-bottom: 18px">' +
+                            '<img class="vk-album-img" src="' + thumb + '">' +
+                            '<div class="form-check form-check-inline"><input class="form-check-input vk-checkbox-video" type="checkbox" data-video-album-id="' + v.id + '" data-owner-id="' + v.owner_id + '" checked></div>' +
+                            '</li>');
+                    });
+                }
+            });
+
+            VK.Api.call('users.get', {
+                user_ids: id,
+                fields: 'bdate,city,country,photo_max_orig,about',
+                v: '5.80'
+            }, function (r) {
+                console.log(r);
+                if (r.response) {
+                    self.userInfo = r.response[0];
                 }
             });
         } else {
             alert("Not authorized");
         }
+    }
+
+    transferAll() {
+        let self = this;
+        let isTransferUserInfo = $('#vkTransferUserInfo').prop('checked');
+        let isTransferPhoto = $('#vkTransferPhoto').prop('checked');
+        let isTransferVideo = $('#vkTransferVideo').prop('checked');
+
+        $('#importFromVKModal').modal('hide');
+        self.main.alert('Development in progress');
+        return;
+
+        if (isTransferUserInfo) {
+            self.main.blog.myProfile.first_name = self.userInfo.first_name;
+            self.main.blog.myProfile.last_name = self.userInfo.last_name;
+            self.main.blog.myProfile.birth_date = self.userInfo.bdate;
+            self.main.blog.myProfile.about = self.userInfo.about;
+            let locationName = self.userInfo.country.title + ', ' + self.userInfo.city.title;
+            self.main.blog.myProfile.location = {name: locationName};
+            // todo download it
+            //self.main.blog.myProfile.photo = self.userInfo.photo_max_orig;
+            self.main.blog.saveProfile(self.main.blog.myProfile).then(function (response) {
+                let data = response.data;
+                self.main.onAfterHashChange(response.data, true);
+                // todo next steps
+            });
+        }
+
+        if (isTransferPhoto) {
+            let photosChecked = $('.vk-checkbox-photo:checked');
+        }
+
+        if (isTransferVideo) {
+            let videosChecked = $('.vk-checkbox-video:checked');
+        }
+
+
     }
 }
 
