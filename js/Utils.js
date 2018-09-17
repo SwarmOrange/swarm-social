b752e1ad9bbefde628fcaaeec1eb46f7f279607f
shadurin <igor.shadurin@gmail.com>
2018-09-17 20:40:13 +0300

    resize image util

---
+++ js/Utils.js
@@ -0,0 +1,53 @@
+class Utils {
+    constructor() {
+    }
+
+    static resizeImages(imageBlob, resultSizes) {
+        return new Promise((resolve, reject) => {
+            let result = {};
+            let urlCreator = window.URL || window.webkitURL;
+            let img = new Image();
+            img.onload = function () {
+                resultSizes.forEach(function (v) {
+                    let canvas = document.createElement("canvas");
+                    let ctx = canvas.getContext("2d");
+                    canvas.width = v.width;
+                    canvas.height = v.height;
+                    if (v.width !== v.height) {
+                        throw  "Method support only square preview";
+                    }
+
+                    let sourceWidth = img.width;
+                    let sourceHeight = img.height;
+                    let sourceXOffset = 0;
+                    let sourceYOffset = 0;
+                    if (img.width > img.height) {
+                        // landscape
+                        sourceWidth = img.height;
+                        sourceHeight = img.height;
+                        //sourceXOffset = v.width / 2;
+                        sourceXOffset = (img.width - img.height) / 2;
+                    } else if (img.width < img.height) {
+                        // portrait
+                        sourceWidth = img.width;
+                        sourceHeight = img.width;
+                        //sourceYOffset = v.width / 2;
+                        sourceYOffset = (img.height - img.width) / 2;
+                    } else {
+                        // square, do nothing
+                    }
+
+                    ctx.drawImage(img, sourceXOffset, sourceYOffset, sourceWidth, sourceHeight, 0, 0, v.width, v.height);
+                    const mimeType = 'image/jpg';
+                    canvas.toBlob((blob) => {
+                        result[v.width + 'x' + v.height] = blob;
+                        resolve(result);
+                    }, mimeType);
+                });
+            };
+            img.src = urlCreator.createObjectURL(imageBlob);
+        });
+    }
+}
+
+module.exports = Utils;
